850ccdef18df8491108d10cd3c1bc268129c9552
Virgile <v.valiau@live.fr>
2021-09-22 09:56:17 +0200

    lab1 triangle à faire

---
+++ src/lab1/functions.py
@@ -0,0 +1,20 @@
+def is_unique(x):
+    for i in range(len(x)):
+        for j in range(i + 1, len(x)):
+            if x[i] == x[j]:
+                return False
+    return True
+
+
+def triangle_shape(height):
+    t = ""
+    if height == 0:
+        return t
+    else:
+        for i in range(height):
+            l = ""
+            for j in range(height - i):
+                l += "x"
+            l += "\n"
+            t += l
+        return t
