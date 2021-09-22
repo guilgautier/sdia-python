32975faf3aef946808e44f83352329a8daa0ef5d
SnowHawkeye <hadriensalem@gmail.com>
2021-09-22 09:41:10 +0200

    Lab 1 - Second session

--- src/lab1/functions.py
+++ src/lab1/functions.py
@@ -1,1 +1,13 @@
-myString = "myString"
+def printMyString():
+    print("myString")
+
+
+def is_unique(x):
+    return len(x) == len(set(x))
+
+
+def triangle_shape(height):
+    str = ""
+    for i in range(0, height):
+        str += "\n" + " " * (height + 1 - i) + "x" * (2 * i + 1)
+    return str
