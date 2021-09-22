62e89eaf49a168b60ea2ae58c48281b0b6852dce
Erik <glebellier@protonmail.com>
2021-09-22 09:54:42 +0200

    Add new is unique function version

--- src/lab1/functions.py
+++ src/lab1/functions.py
@@ -1,11 +1,17 @@
 def is_unique(x):
-    while x!=[]:
-        elt  = x.pop()
+    while x != []:
+        elt = x.pop()
         if elt in x: return False
     return True
 
+
+def is_unique2(x):
+    return len(x) == len(set(x))
+
+
 def triangle_shape(n):
-    base_n = 2*n-1
+    base_n = 2 * n - 1
     for i in range(1, n):
-        step_n = 2*i-1
-        print(" " * ((base_n-step_n)//2) + "*"*step_n + " " * ((base_n-step_n)//2))
+        step_n = 2 * i - 1
+        print(" " * ((base_n - step_n) // 2) + "*" * step_n + " " *
+              ((base_n - step_n) // 2))
