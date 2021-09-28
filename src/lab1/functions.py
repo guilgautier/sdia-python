2685312a0155cdcbd91baa74935958c234c76e76
AnnaMarizy <marizyanna74@gmail.com>
2021-09-28 10:01:21 +0200

    Lab2-mardi

--- src/lab1/functions.py
+++ src/lab1/functions.py
@@ -1,20 +1,43 @@
 def is_unique(x):
-    """returns True if the there is no duplicate items otherwise return False.
+    """Returns True if there is no duplicate in x otherwise return False.
+
     Args:
+        x (list) : collection of elements
+
+    Returns:
+        boolean: True/False to know if there is duplicates
 
     """
+
     for value in x:
         if x.count(value) > 1:
             return False
     return True
 
 
+def is_unique2(x):
+    """Returns True if there is no duplicate in x otherwise return False.
+
+    Args:
+        x (list): collection of elements
+
+    Returns:
+        Boolean
+    """
+
+    return len(set(x)) == len(x)
+
+
 def triangle_shape(height):
-    """returns a string that forms a triangle with height prescribed by `height`, or "" if height=0
+    """Returns a string that forms a triangle with height prescribed by `height`, or '' if height=0
 
     Args:
         height (int): height of the pyramid
+
+    Returns:
+        str: Representation of a pyramid
     """
+
     pyramid = ""
     if height == 0:
         return pyramid
@@ -22,4 +45,4 @@ def triangle_shape(height):
         pyramid += (
             (height - i - 1) * " " + (2 * i + 1) * "x" + (height - i - 1) * " " + "\n"
         )
-    return pyramid
+    return pyramid[:-1]
