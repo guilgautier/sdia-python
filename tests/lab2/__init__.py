d9a3d380cd4c7157059ac43f534489ce38fd3ecc
Guillaume Gautier <guillaume.gga@gmail.com>
2021-10-02 17:51:31 +0200

    Format files

--- tests/lab2/__init__.py
+++ tests/lab2/__init__.py
@@ -12,7 +12,11 @@ def example_box_window():
 
 @pytest.mark.parametrize(
     "point, expected",
-    [(np.array([0, 0]), True), (np.array([-1, 5]), True), (np.array([5, 6]), False),],
+    [
+        (np.array([0, 0]), True),
+        (np.array([-1, 5]), True),
+        (np.array([5, 6]), False),
+    ],
 )
 def test_indicator_function_box_2d(example_box_window, point, expected):
     box = example_box_window
