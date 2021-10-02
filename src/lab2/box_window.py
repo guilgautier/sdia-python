9983c150338ff2801fffb86a59beaed598876984
Justine Brgn <justine.bourgain.99@gmail.com>
2021-10-02 16:32:11 +0200

    première tentative tp2 tests

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -1,47 +1,89 @@
 from lab2.utils import get_random_number_generator
+#import numpy as np
+
 
 
 class BoxWindow:
-    """[summary]"""
+    """Creates a window of rectangular shape"""
 
     def __init__(self, args):
-        """[summary]
+        """initialize the box window with the bounding points
 
         Args:
-            args ([type]): [description]
+            args (np.array([integer])): array of the bounding points of the box
         """
-        self.bounds = None
+        self.bounds = args
+
 
     def __str__(self):
         r"""BoxWindow: :math:`[a_1, b_1] \times [a_2, b_2] \times \cdots`
 
         Returns:
-            [type]: [description]
+            str : give the bounds of the box
         """
-        return ""
+        mot=""
+        for [a,b] in self.bounds:
+            mot=mot+str([a,b])+' x '
+        return ("BoxWindow: " + mot[:-3])
+
+
+
 
     def __len__(self):
-        return len(self)
+        """
+        Returns:
+            list : list of the length in each direction
+        """
+        L=[]
+        for (a,b) in self.bounds:
+            L.append(b-a)
+        return L
 
-    def __contains__(self, args):
-        # x in my_box_window
-        return True or False
 
-    def dimension(self):
-        """[summary]"""
-        return
-
-    def volume(self):
-        """[summary]"""
-        return
-
-    def indicator_function(self, args):
-        """[summary]
+    def __contains__(self, point):
+        """Indicates if the point is contained in the box window.
+        Returns True if the point is in the box, returns False otherwise.
 
         Args:
-            args ([type]): [description]
+            point ([np.array([Integer])): coordonnées d'un point
         """
-        return
+        for (a,b),x in zip(self.bounds, point):
+            if not (a<=x<=b):
+                return False
+        return True
+
+
+
+    def dimension(self):
+        """
+        Returns:
+            integer : dimension of the box
+        """
+        return (len(self.bounds))
+
+
+    def volume(self):
+        """
+        Returns:
+            integer : volume of the box
+        """
+        vol=1
+        for p in self.__len__():
+            vol=vol*p
+        return vol
+
+
+    def indicator_function(self, args):
+        """Indicator function of the box window. Returns 1 if the point is in the box, returns 0 otherwise.
+
+        Args:
+            args ([np.array([Integer])): coordonnées d'un point
+        """
+        if self.__contains__(args)==True:
+            return (1)
+        else:
+            return (0)
+
 
     def rand(self, n=1, rng=None):
         """Generate ``n`` points uniformly at random inside the :py:class:`BoxWindow`.
@@ -51,8 +93,17 @@ class BoxWindow:
             rng ([type], optional): [description]. Defaults to None.
         """
         rng = get_random_number_generator(rng)
-        #rng.random (doc numpy)
-        return
+
+        L=[]
+        for p in range(n):
+            L_petit=[]
+            for (a,b) in self.bounds:
+                if a==b:
+                    L_petit.append(a)
+                else:
+                    L_petit.append(np.random.uniform(b-a)+a)
+            L.append(L_petit)
+        return (L)
 
 
 #heritage
--- tests/lab2/test_box_window.py
+++ tests/lab2/test_box_window.py
@@ -47,3 +47,159 @@ def test_indicator_function_box_2d(box_2d_05, point, expected):
 # ================================
 # ==== WRITE YOUR TESTS BELOW ====
 # ================================
+
+
+
+def test_init ():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    assert c.bounds.shape==(3,2)
+
+@pytest.mark.parametrize(
+    "bounds, expected",
+    [
+        (np.array([[0, 5], [-1.45, 3.14], [-10, 10]]), (3,2)),
+        (np.array([2.5, 2.5]), (1,2)),
+    ],
+)
+def test_init2(bounds, expected):
+    c=BoxWindow(bounds)
+    assert c.bounds.shape == expected
+
+
+
+
+
+
+
+def test_len():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    assert c.__len__() == [5.0, 4.59, 20.0]
+
+@pytest.mark.parametrize(
+    "bounds, expected",
+    [
+        (np.array([[0, 5], [0, 5]]), [5,5]),
+        (np.array([2.5, 2.5]), [0]),
+    ],
+)
+def test_len2(bounds, expected):
+    c=BoxWindow(bounds)
+    assert c.__len__() == expected
+
+
+
+
+
+
+
+def test_contains():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    point1=np.array([1, 0, 0])
+    assert (c.__contains__(point1)) == True
+
+
+@pytest.fixture
+def box_2d_05():
+    return BoxWindow(np.array([[0, 5], [0, 5]]))
+
+
+@pytest.mark.parametrize(
+    "point, expected",
+    [
+        (np.array([1, 1]), True),
+        (np.array([2.5, 2.5]), True),
+        (np.array([-1, 5]), False),
+        (np.array([10, 3]), False),
+    ],
+)
+def test_contains2(box_2d_05, point, expected):
+    is_in = box_2d_05.__contains__(point)
+    assert is_in == expected
+
+
+
+
+
+
+def test_dimension():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    assert c.dimension() == 3
+
+@pytest.mark.parametrize(
+    "bounds, expected",
+    [
+        (np.array([[0, 5], [0, 5]]), 2),
+        (np.array([2.5, 2.5]), 1),
+    ],
+)
+def test_dimension2(bounds, expected):
+    c=BoxWindow(bounds)
+    assert c.dimension() == expected
+
+
+
+
+
+def test_volume():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    assert (c.volume()) == 459
+
+@pytest.mark.parametrize(
+    "bounds, expected",
+    [
+        (np.array([[0, 5], [0, 5]]), 25),
+        (np.array([2.5, 2.5]), 0),
+    ],
+)
+def test_volume2(bounds, expected):
+    c=BoxWindow(bounds)
+    assert c.volume() == expected
+
+
+
+
+
+def test_indicator_function():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    point1=np.array([1, 0, 0])
+    assert (c.indicator_function(point1)) == 1
+
+
+@pytest.fixture
+def box_2d_05():
+    return BoxWindow(np.array([[0, 5], [0, 5]]))
+
+
+@pytest.mark.parametrize(
+    "point, expected",
+    [
+        (np.array([1, 1]), 1),
+        (np.array([2.5, 2.5]), 1),
+        (np.array([-1, 5]), 0),
+        (np.array([10, 3]), 0),
+    ],
+)
+def test_indicator_function2(box_2d_05, point, expected):
+    is_in = box_2d_05.indicator_function(point)
+    assert is_in == expected
+
+
+
+
+
+
+def test_rand():
+    c=BoxWindow(np.array([[0, 5], [-1.45, 3.14], [-10, 10]]))
+    assert c.__contains__(c.rand(1)) == True
+
+
+@pytest.mark.parametrize(
+    "bounds, expected",
+    [
+        (np.array([[0, 5], [0, 5]]), True),
+        (np.array([2.5, 2.5]), True),
+    ],
+)
+def test_rand2(bounds, expected):
+    c=BoxWindow(bounds)
+    assert c.__contains__(c.rand(1)) == expected
