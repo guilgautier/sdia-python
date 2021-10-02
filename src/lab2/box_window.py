8b6e677be189599065f3c5076500fcfe1e3d736e
Guillaume Gautier <guillaume.gga@gmail.com>
2021-10-02 21:06:05 +0200

    Fix call to super in init of UnitBoxWindow

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -61,4 +61,4 @@ class UnitBoxWindow(BoxWindow):
             dimension ([type]): [description]
             center ([type], optional): [description]. Defaults to None.
         """
-        super(BoxWindow, self).__init__(args)
+        super(UnitBoxWindow, self).__init__(bounds)
