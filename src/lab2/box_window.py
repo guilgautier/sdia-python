705ca1da831a794a7548154a8b5be453e7195943
Guillaume Gautier <guillaume.gga@gmail.com>
2021-10-05 10:53:57 +0200

    Feedback on lab2

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -1,7 +1,10 @@
-from lab2.utils import get_random_number_generator
 import numpy as np
 
+from lab2.utils import get_random_number_generator
 
+
+# todo write and clean up the docstrings [ ]
+# todo write and run tests
 class BoxWindow:
     """[Simple class describing a box]"""
 
@@ -19,10 +22,13 @@ class BoxWindow:
         Returns:
             [type]: [description]
         """
+        # ! use f-strings
         bounds_list = []
         if len(self.bounds.shape) == 1:
             return "BoxWindow: " + str(self.bounds.tolist())
 
+        # * consider for i, (a, b) in enumerate(self.bounds)
+        # * use += operator
         A_list = self.bounds.tolist()
         to_print = ""
         for i in range(len(A_list)):
@@ -39,13 +45,15 @@ class BoxWindow:
         Returns:
             [type]: [description]
         """
-        S = 0
+        S = 0  # ! naming: S -> length for example
         for segment in self.bounds:
             S += segment[1] - segment[0]
         return S
 
     def __contains__(self, point):
 
+        # * consider for (a, b), x in zip(self.bounds, point)
+        # * or exploit numpy vectorization power
         for i in range(len(point)):
             if not (self.bounds[i][0] <= point[i] <= self.bounds[i][1]):
                 return False
@@ -53,10 +61,11 @@ class BoxWindow:
 
     def dimension(self):
         """[Returns the mathematical dimension of the box]"""
+        # * nice use of .shape
         return self.bounds.shape[0]
 
     def volume(self):
-        produit = 1
+        produit = 1  # ? naming: produit -> volume
         for segment in self.bounds:
             longueur = segment[1] - segment[0]
             produit *= longueur
@@ -68,6 +77,8 @@ class BoxWindow:
         Args:
             args ([type]): [the point must have the same dimension as the box]
         """
+        # ? how would you handle multiple points
+        # todo readability consider using "point in self"
         verite = self.__contains__(point)
         return verite
 
@@ -79,6 +90,8 @@ class BoxWindow:
         """
         rng = get_random_number_generator(rng)
         number_list = []
+        # * exploit numpy, rng.uniform(a, b, size=n)
+        # * consider for a, b in self.bounds
         for bound in self.bounds:
             number_list.append(rng.uniform(bound[0], bound[1]))
         return np.array(number_list)
@@ -90,10 +103,14 @@ class BoxWindow:
             n (int, optional): [description]. Defaults to 1.
             rng ([type], optional): [description]. Defaults to None.
         """
+        # * interesting way to decouple rand and rand_n
+        # ? why passing a default n=1 argument if not calling self.rand when n=1
         points_list = [self.rand(rng) for i in range(n)]
         return points_list
+        # ? why returning a list and not an np.array
 
 
+# todo class UnitBoxWindow and BallWindow not defined
 class UnitBoxWindow(BoxWindow):
     def __init__(self, center, dimension):
         """[summary]
@@ -102,4 +119,4 @@ class UnitBoxWindow(BoxWindow):
             dimension ([type]): [description]
             center ([type], optional): [description]. Defaults to None.
         """
-        super(BoxWindow, self).__init__(args)
+        super(UnitBoxWindow, self).__init__(args)
